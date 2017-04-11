c363eb267c2cf3a9eaed5747ab5609b10d228b95
root <rvpanoz@gmail.com>
2017-04-12 00:43:48 +0300

    clean up

--- src/views/tracks/track.js
+++ src/views/tracks/track.js
@@ -36,17 +36,13 @@ var TrackView = Marionette.View.extend({
 	},
 	initialize(params) {
 		_.bindAll(this, 'onStopAnimation', 'updateProgress');
-
 		//get track id
 		var trackId = _.get(params, 'trackId');
-
 		//Instatiate a new track
 		this.model = new Schema.Track({
 			id: trackId
 		});
-
 		this.listenTo(this, 'stop:animation', this.onStopAnimation, arguments, this)
-
 		//fetch track
 		this.model.fetch();
 	},
@@ -69,162 +65,103 @@ var TrackView = Marionette.View.extend({
 	},
 
 	onDomRefresh() {
-		if (this.model.get('stream_url')) {
-			_.delay(_.bind(function () {
-				this.duration = this.audioElement[0].duration;
-				var finishTime = this.duration * 1000;
-				var timeFinish = moment.duration(finishTime);
-				this.getUI('timeFinish').text(moment.utc(timeFinish.asMilliseconds()).format("HH:mm:ss"));
-			}, this), 1000);
+    if (this.model.get('stream_url')) {
+      _.delay(_.bind(function() {
+        this.duration = this.audioElement[0].duration;
+        var finishTime = this.duration * 1000; //milliseconds
+        var timeFinish = moment.duration(finishTime);
+        this.getUI('timeFinish').text(moment.utc(timeFinish.asMilliseconds()).format("HH:mm:ss"));
+      }, this), 3000);
 
-			//add events
-			this.audioElement[0].addEventListener("progress", this.updateProgress, arguments);
-			this.audioElement[0].addEventListener("canplay", _.bind(function () {}, this));
-			this.player = this.getUI('player');
+      //draw the graph
+      this.draw();
+    }
 
-			//draw the graph
-			this.draw();
-		}
-	},
+    //add events
+    this.audioElement[0].addEventListener("progress", this.updateProgress, arguments);
+    this.audioElement[0].addEventListener("canplay", _.bind(function() {}, this));
+    this.player = this.getUI('player');
+  },
 
-	onStopAnimation(e) {
-		if (this.timer) {
-			this.timer.stop();
-		}
-		return false;
-	},
+  onStopAnimation(e) {
+		if(this.timer)
+    	this.timer.stop();
+    return false;
+  },
 
-	draw() {
-		var hh = $('nav').height();
-		var w = this.getUI('planetarium').width() - 50;
-		var h = $(window).height() - (3 * hh);
-		var x = w / 2;
-		var y = h / 2;
-		var t0 = new Date();
+  draw() {
+    var hh = $('nav').height();
+    var w = this.getUI('planetarium').width();
+    var h = $(window).height() - (3 * hh);
+    var x = w / 2;
+    var y = h / 2;
+    var t0 = new Date();
+    var delta = Date.now() - t0;
 
-		var radius = Math.min(w, h);
+    var _this = this;
 
-		//planets data
-		var data = require('assets/files/planets-simple');
+    // the data to visualize
+    this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+    this.planets = require('assets/files/planets-simple');
 
-		var color = d3.scale.category20c();
+    // the planetarium
+    this.planetarium = d3.select(this.svgContainer)
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
 
-		var rads = {
-			'sun': radius / 8,
-			earthOrbit: radius / 2.5,
-			earth: radius / 32,
-			moonOrbit: radius / 16,
-			moon: radius / 96
-		};
+    // the sun
+    this.planetarium
+      .append("circle")
+      .attr("r", 20)
+      .attr("cx", x)
+      .attr("cy", y)
+      .attr("class", "sun");
 
-		// initialize board
-		this.spaceBoard = d3.select(this.svgContainer)
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h)
-			.append("g")
-			.attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+    // planet group
+    var planetGroup = this.planetarium.append("g")
+      .attr("class", "planet-group")
+      .attr("transform", "translate(" + x + "," + y + ")");
 
-		//the sun
-		this.spaceBoard.append("circle")
-			.attr("class", "sun")
-			.attr("r", rads.sun)
-			.style("fill", "rgba(255, 204, 0, 1.0)");
+    // draw planets with orbits
+    planetGroup.selectAll("g.planet")
+      .data(this.planets)
+      .enter()
+      .append("g")
+      .attr("class", "planet-cluster").each(function(d, i) {
+        d3.select(this).append("circle")
+          .attr("class", "orbit")
+          .attr("r", d.R * 2)
+          .attr("cx", 0)
+          .attr("cy", 0)
+        d3.select(this).append("circle")
+          .attr("r", d.r)
+          .attr("cx", d.R * 2)
+          .attr("class", "planet");
+      });
+  },
 
-		// Earth's orbit
-		this.spaceBoard.append("circle")
-			.attr("class", "earthOrbit")
-			.attr("r", rads.earthOrbit)
-			.style("fill", "none")
-			.style("stroke", "rgba(255, 204, 0, 0.25)");
+  play: function(e) {
+    var t0 = new Date();
+    var delta = (Date.now() - t0);
+    var _this = this;
 
-		var earthOrbitPosition = d3.arc()
-			.outerRadius(rads.earthOrbit + 1)
-			.innerRadius(rads.earthOrbit - 1)
-			.startAngle(0)
-			.endAngle(0);
+    //start playing
+    this.audioElement[0].play();
+    // this.audioElement[0].volume = 0.01;
 
-		this.spaceBoard.append("path")
-			.attr("class", "earthOrbitPosition")
-			.attr("d", earthOrbitPosition)
-			.style("fill", "rgba(255, 204, 0, 0.75)");
-
-		// Earth
-		this.spaceBoard.append("circle")
-			.attr("class", "earth")
-			.attr("r", rads.earth)
-			.attr("transform", "translate(0," + -rads.earth + ")")
-			.style("fill", "rgba(113, 170, 255, 1.0)");
-
-		// Moon's orbit
-		this.spaceBoard.append("circle")
-			.attr("class", "moonOrbit")
-			.attr("r", rads.moonOrbit)
-			.attr("transform", "translate(0," + -rads.earthOrbit + ")")
-			.style("fill", "none")
-			.style("stroke", "rgba(113, 170, 255, 0.25)");
-
-		// Current position of the Moon in its orbit
-		var moonOrbitPosition = d3.arc()
-			.outerRadius(rads.moonOrbit + 1)
-			.innerRadius(rads.moonOrbit - 1)
-			.startAngle(0)
-			.endAngle(0);
-		this.spaceBoard.append("path")
-			.attr("class", "moonOrbitPosition")
-			.attr("d", moonOrbitPosition)
-			.attr("transform", "translate(0," + -rads.earthOrbit + ")")
-			.style("fill", "rgba(113, 170, 255, 0.75)");
-
-		// Moon
-		this.spaceBoard.append("circle")
-			.attr("class", "moon")
-			.attr("r", rads.moon)
-			.attr("transform", "translate(0," + (-rads.earthOrbit + -rads.moonOrbit) + ")")
-			.style("fill", "rgba(150, 150, 150, 1.0)");
-	},
-
-	play: function (e) {
-		var _this = this;
-		var t0 = new Date();
-		var speed = 0.4;
-
-		//start playing
-		this.audioElement[0].play();
-
-		//get frequency data
-		// _this.analyser.getByteTimeDomainData(dataArray);
-		// _this.analyser.getByteFrequencyData(dataArray);
-
-		var bufferLength = this.analyser.frequencyBinCount;
-		var dataArray = new Uint8Array(bufferLength);
-		var line = d3.line();
-
-		//sort data
-		dataArray.sort();
-
-		//start timer
-		this.timer = d3.timer(function () {
-			var delta = (Date.now() - t0) / 1000; //seconds
-
-			//start rotation
-			_this.spaceBoard.selectAll(".moon")
-				.data(dataArray)
-				.transition()
-				.delay(function (d, i, nodes) {
-					return delta * 0.5;
-				})
-				.attr("transform", function (d, i) {
-					return "rotate(" + d / 2 + ")";
-				})
-
-			//clean up
-			_this.spaceBoard.exit().remove();
-		});
-
-		this.getUI('stop').removeClass('hide');
-		this.getUI('play').addClass('hide');
-	},
+    //start timer
+    this.timer = d3.timer(function() {
+      var delta = (Date.now() - t0);
+      var speed = 0;
+      _this.planetarium.selectAll(".planet-cluster").attr("transform", function(d) {
+        speed = d.phi0 * (delta / 3600);
+        return "rotate(" + speed + ")";
+      });
+    });
+    this.getUI('stop').removeClass('hide');
+    this.getUI('play').addClass('hide');
+  },
 
 	stop: function (e) {
 		this.audioElement[0].pause();
@@ -249,7 +186,7 @@ var TrackView = Marionette.View.extend({
 		var timeNow = moment.duration(currenTimeMilli);
 
 		if (progress.length && currentTime > 0) {
-			value = Math.floor(100 * currentTime / duration);
+			value = Math.floor(10 * currentTime / duration);
 			timeValue = Math.floor(currentTime);
 
 			progress.css({
